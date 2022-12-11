a7ea128df0843257e63e8a99dad1517ae3e41c5c
Brian <nieh_brian@yahoo.com>
2022-12-10 19:12:24 -0800

    Test compile

--- test.cpp
+++ test.cpp
@@ -8,5 +8,8 @@ int main()
     DoublyLinkedList<std::string> DLL;
     readData(DLL);
     DLL.print();
+    DLL.searchDelete("cir");
+    DLL.searchDelete("his");
+    DLL.print();
     return 0;
 }
